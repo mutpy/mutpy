f0f7ca26f0f5502d897005ac39301341ac6052cb
khalas <khalas@localhost>
2011-05-12 11:29:06 +0200

    Create package structure inside temporary directory.

--- mutpy/test/controller_test.py
+++ mutpy/test/controller_test.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import shutil
 import types
+import tempfile
+import sys
 
 from mutpy import controller
 
@@ -10,11 +12,13 @@ class ModulesLoaderTest(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        os.makedirs('_a/b/c')
-        open('_a/__init__.py', 'w').close()
-        open('_a/b/__init__.py', 'w').close()
-        open('_a/b/c/__init__.py', 'w').close()
-        with open('_a/b/c/sample.py', 'w') as f:
+        cls.tmp = tempfile.mkdtemp(prefix='mutpytmp-') + '/'
+        sys.path.append(cls.tmp)
+        os.makedirs(cls.tmp + 'a/b/c')
+        open(cls.tmp + 'a/__init__.py', 'w').close()
+        open(cls.tmp + 'a/b/__init__.py', 'w').close()
+        open(cls.tmp + 'a/b/c/__init__.py', 'w').close()
+        with open(cls.tmp + 'a/b/c/sample.py', 'w') as f:
             f.write('class X:\n\tdef f():\n\t\tpass')
             
     
@@ -22,44 +26,44 @@ class ModulesLoaderTest(unittest.TestCase):
         self.loader = controller.ModulesLoader(None, None)
         
     def test_load_file(self):
-        module, to_mutate = self.loader.load('_a/b/c/sample.py')
+        module, to_mutate = self.loader.load(self.__class__.tmp + 'a/b/c/sample.py')
         
         self.assertIsNone(to_mutate)
         self.assertIsInstance(module, types.ModuleType)
         self.assertTrue(hasattr(module, 'X'))
-        self.assertMultiLineEqual(module.__file__, '_a/b/c/sample.py')
+        self.assertMultiLineEqual(module.__file__, self.__class__.tmp + 'a/b/c/sample.py')
         self.assertMultiLineEqual(module.__name__, 'sample')
         
     def test_load_module(self):
-        module, to_mutate = self.loader.load('_a.b.c.sample')
+        module, to_mutate = self.loader.load('a.b.c.sample')
         
         self.assertIsNone(to_mutate)
         self.assertIsInstance(module, types.ModuleType)
         self.assertTrue(hasattr(module, 'X'))
-        self.assertMultiLineEqual(module.__file__, '_a/b/c/sample.py')
-        self.assertMultiLineEqual(module.__name__, '_a.b.c.sample')
+        self.assertMultiLineEqual(module.__file__, self.__class__.tmp + 'a/b/c/sample.py')
+        self.assertMultiLineEqual(module.__name__, 'a.b.c.sample')
         
     def test_target_class(self):
-        module, to_mutate = self.loader.load('_a.b.c.sample.X')
+        module, to_mutate = self.loader.load('a.b.c.sample.X')
         
         self.assertMultiLineEqual(to_mutate, 'X')
         self.assertIsInstance(module, types.ModuleType)
         self.assertTrue(hasattr(module, 'X'))
-        self.assertMultiLineEqual(module.__file__, '_a/b/c/sample.py')
-        self.assertMultiLineEqual(module.__name__, '_a.b.c.sample')
+        self.assertMultiLineEqual(module.__file__, self.__class__.tmp + 'a/b/c/sample.py')
+        self.assertMultiLineEqual(module.__name__, 'a.b.c.sample')
     
     def test_target_method(self):
-        module, to_mutate = self.loader.load('_a.b.c.sample.X.f')
+        module, to_mutate = self.loader.load('a.b.c.sample.X.f')
         
         self.assertMultiLineEqual(to_mutate, 'X.f')
         self.assertIsInstance(module, types.ModuleType)
         self.assertTrue(hasattr(module, 'X'))
-        self.assertMultiLineEqual(module.__file__, '_a/b/c/sample.py')
-        self.assertMultiLineEqual(module.__name__, '_a.b.c.sample')
+        self.assertMultiLineEqual(module.__file__, self.__class__.tmp + 'a/b/c/sample.py')
+        self.assertMultiLineEqual(module.__name__, 'a.b.c.sample')
         
     def test_bad_target(self):
-        self.assertRaises(controller.ModulesLoaderException, lambda : self.loader.load('_a.b.c.sample.Y'))
+        self.assertRaises(controller.ModulesLoaderException, lambda : self.loader.load('a.b.c.sample.Y'))
         
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree('_a')
+        shutil.rmtree(cls.tmp)
