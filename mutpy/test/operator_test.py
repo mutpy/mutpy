c9bf8828b172ee45b510b8e1f699d6fe5a1f2cba
Konrad Hałas <halas.konrad@gmail.com>
2011-05-22 21:02:12 +0200

    Added ArithmeticOperatorReplacement, BinaryOperatorReplacement, LogicalOperatorReplacement and ConditionalOperatorReplacement with tests.

--- mutpy/test/operator_test.py
+++ mutpy/test/operator_test.py
@@ -11,12 +11,13 @@ class MutationOperatorTest(unittest.TestCase):
 		class X:
 			def visit_A(self): pass
 			def visit_A_1(self): pass
-			def visit_A2(self): pass
+			def visit_A_2(self): pass
+			def visit_AB(self): pass
 			def visitA(self): pass
 			def visit_B(self): pass
 		
 		x = X()
-		visits_method = ['visit_A', 'visit_A_1', 'visit_A2']
+		visits_method = ['visit_A', 'visit_A_1', 'visit_A_2']
 		
 		for attr in operator.MutationOperator.getattr_like(x, 'visit_A'):
 			self.assertIn(attr.__name__, visits_method)
@@ -70,6 +71,74 @@ class ArithmeticOperatorReplacementTest(OperatorTestCase):
 	def test_add_to_sub_replacement(self):
 		self.assert_mutation('x + y + z', ['x - y + z', 'x + y - z'])
 		
+	def test_sub_to_add_replacement(self):
+		self.assert_mutation('x - y', ['x + y'])
+		
+	def test_mult_to_div_replacement(self):
+		self.assert_mutation('x * y', ['x / y', 'x // y'])
+		
+	def test_div_replacement(self):
+		self.assert_mutation('x / y', ['x * y', 'x // y'])
+		
+	def test_floor_div_to_div(self):
+		self.assert_mutation('x // y', ['x / y', 'x * y'])	
+
+	def test_mod_to_mult(self):
+		self.assert_mutation('x % y', ['x * y'])
+		
+
+class BinaryOperatorReplacement(OperatorTestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		cls.op = operator.BinaryOperatorReplacement()
+		
+	def test_bin_and_to_bit_or(self):
+		self.assert_mutation('x & y', ['x | y'])
+	
+	def test_bit_or_to_bit_and(self):
+		self.assert_mutation('x | y', ['x & y'])
+		
+	def test_bit_xor_to_bit_and(self):
+		self.assert_mutation('x ^ y', ['x & y'])
+		
+	def test_lshift_to_rshift(self):
+		self.assert_mutation('x << y', ['x >> y'])
+		
+	def test_rshift_to_lshift(self):
+		self.assert_mutation('x >> y', ['x << y'])
+		
+
+class LogicaOperatorReplacementTest(OperatorTestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		cls.op = operator.LogicaOperatorReplacement()
+		
+	def test_and_to_or(self):
+		self.assert_mutation('(x and y)', ['(x or y)'])
+		
+	def test_or_to_and(self):
+		self.assert_mutation('(x or y)', ['(x and y)'])
+		
+		
+class ConditionalOperatorReplacementTest(OperatorTestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		cls.op = operator.ConditionalOperatorReplacement()
+		
+	def test_lt(self):
+		self.assert_mutation('x < y', ['x > y', 'x <= y'])
+		
+	def test_gt(self):
+		self.assert_mutation('x > y', ['x < y', 'x >= y'])
+		
+	def test_lte(self):
+		self.assert_mutation('x <= y', ['x >= y', 'x < y'])
+		
+	def test_gte(self):
+		self.assert_mutation('x >= y', ['x <= y', 'x > y'])
 
 class SliceIndexReplaceTest(OperatorTestCase):
 	
