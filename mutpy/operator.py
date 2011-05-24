fc594f6e656ba38959cd702c176c4a5f2233301e
Konrad Hałas <halas.konrad@gmail.com>
2011-05-25 01:14:46 +0200

    Changed visit_ to mutate_.

--- mutpy/operator.py
+++ mutpy/operator.py
@@ -13,14 +13,14 @@ class MutationOperator(ast.NodeTransformer):
     
     def mutate(self, node, to_mutate):
         self.muteted_node_number = 0
-        self.visit_method_number = 0
+        self.mutate_method_number = 0
         self.mutation_flag = False
         while True:
             self.visited_node_number = 0
             node_copy = copy.deepcopy(node)
             new_node = self.visit(node_copy)
             
-            if not self.visit_method_number:
+            if not self.mutate_method_number:
                 self.muteted_node_number += 1
                 
             if not self.mutation_flag:
@@ -53,13 +53,13 @@ class MutationOperator(ast.NodeTransformer):
                 self.visited_node_number += 1
                 new_node = self.generic_visit(node)
             else:
-                new_node = self.visit_with_visitors(node, visitors)
+                new_node = self.mutate_with_visitors(node, visitors)
                     
         return new_node
     
-    def visit_with_visitors(self, node, visitors):
-        while self.visit_method_number < len(visitors):
-            visitor = visitors[self.visit_method_number]
+    def mutate_with_visitors(self, node, visitors):
+        while self.mutate_method_number < len(visitors):
+            visitor = visitors[self.mutate_method_number]
             try:
                 new_node = visitor(node)
                 self.mutate_lineno = node.lineno if hasattr(node, 'lineno') else self.curr_line
@@ -67,17 +67,17 @@ class MutationOperator(ast.NodeTransformer):
                 if new_node:
                     ast.copy_location(new_node, node)
                 if visitor is visitors[-1]:
-                    self.visit_method_number = 0
+                    self.mutate_method_number = 0
                     self.visited_node_number += 1
                 else:
-                    self.visit_method_number += 1
+                    self.mutate_method_number += 1
                     
                 break
             except MutationResign:
-                self.visit_method_number += 1
+                self.mutate_method_number += 1
                 self.muteted_node_number += 1
         else:
-            self.visit_method_number = 0
+            self.mutate_method_number = 0
             self.visited_node_number += 1
             new_node = self.generic_visit(node)
         
@@ -93,119 +93,119 @@ class MutationOperator(ast.NodeTransformer):
         return ''.join([c for c in cls.__name__ if str.isupper(c)])
 
     def find_visitors(self, node):
-        method_prefix = 'visit_' + node.__class__.__name__
+        method_prefix = 'mutate_' + node.__class__.__name__
         visitors = MutationOperator.getattr_like(self, method_prefix)
         return visitors
   
 
 class ArithmeticOperatorReplacement(MutationOperator):
     
-    def visit_Add(self, node):
+    def mutate_Add(self, node):
         return ast.Sub()
     
-    def visit_Sub(self, node):
+    def mutate_Sub(self, node):
         return ast.Add()
     
-    def visit_Mult_to_Div(self, node):
+    def mutate_Mult_to_Div(self, node):
         return ast.Div()
     
-    def visit_Mult_to_FloorDiv(self, node):
+    def mutate_Mult_to_FloorDiv(self, node):
         return ast.FloorDiv()
     
-    def visit_Div_to_Mult(self, node):
+    def mutate_Div_to_Mult(self, node):
         return ast.Mult()
     
-    def visit_Div_to_FloorDiv(self, node):
+    def mutate_Div_to_FloorDiv(self, node):
         return ast.FloorDiv()
     
-    def visit_FloorDiv_to_Div(self, node):
+    def mutate_FloorDiv_to_Div(self, node):
         return ast.Div()
     
-    def visit_FloorDiv_to_Mult(self, node):
+    def mutate_FloorDiv_to_Mult(self, node):
         return ast.Mult()
     
-    def visit_Mod(self, node):
+    def mutate_Mod(self, node):
         return ast.Mult()
     
 
 class BinaryOperatorReplacement(MutationOperator):
     
-    def visit_BitAnd(self, node):
+    def mutate_BitAnd(self, node):
         return ast.BitOr()
     
-    def visit_BitOr(self, node):
+    def mutate_BitOr(self, node):
         return ast.BitAnd()
     
-    def visit_BitXor(self, node):
+    def mutate_BitXor(self, node):
         return ast.BitAnd()
     
-    def visit_LShift(self, node):
+    def mutate_LShift(self, node):
         return ast.RShift()
     
-    def visit_RShift(self, node):
+    def mutate_RShift(self, node):
         return ast.LShift()
     
 
 class LogicaOperatorReplacement(MutationOperator):
     
-    def visit_And(self, node):
+    def mutate_And(self, node):
         return ast.Or()
     
-    def visit_Or(self, node):
+    def mutate_Or(self, node):
         return ast.And()
 
 
 class ConditionalOperatorReplacement(MutationOperator):
     
-    def visit_Lt(self, node):
+    def mutate_Lt(self, node):
         return ast.Gt()
     
-    def visit_Lt_to_LtE(self, node):
+    def mutate_Lt_to_LtE(self, node):
         return ast.LtE()
         
-    def visit_Gt(self, node):
+    def mutate_Gt(self, node):
         return ast.Lt()
     
-    def visit_Gt_to_GtE(self, node):
+    def mutate_Gt_to_GtE(self, node):
         return ast.GtE()
     
-    def visit_LtE(self, node):
+    def mutate_LtE(self, node):
         return ast.GtE()
     
-    def visit_LtE_to_Lt(self, node):
+    def mutate_LtE_to_Lt(self, node):
         return ast.Lt()
     
-    def visit_GtE(self, node):
+    def mutate_GtE(self, node):
         return ast.LtE()
     
-    def visit_GtE_to_Gt(self, node):
+    def mutate_GtE_to_Gt(self, node):
         return ast.Gt()
     
-    def visit_Eq(self, node):
+    def mutate_Eq(self, node):
         return ast.NotEq()
 
-    def visit_NotEq(self, node):
+    def mutate_NotEq(self, node):
         return ast.Eq()
 
 
 class UnaryOperatorReplacement(MutationOperator):
     
-    def visit_USub(self, node):
+    def mutate_USub(self, node):
         return ast.UAdd()
     
-    def visit_UAdd(self, node):
+    def mutate_UAdd(self, node):
         return ast.USub()
     
         
 class ConstantReplacement(MutationOperator):
     
-    def visit_Num(self, node):
+    def mutate_Num(self, node):
         return ast.Num(n=node.n + 1)
     
-    def visit_Str(self, node):
+    def mutate_Str(self, node):
         return ast.Str(s='mutpy')
     
-    def visit_Str_empty(self, node):
+    def mutate_Str_empty(self, node):
         if not node.s:
             raise MutationResign()
         
@@ -214,13 +214,13 @@ class ConstantReplacement(MutationOperator):
 
 class StatementDeletion(MutationOperator):
     
-    def visit_Assign(self, node):
+    def mutate_Assign(self, node):
         return ast.Pass()
     
-    def visit_Return(self, node):
+    def mutate_Return(self, node):
         return ast.Pass()
     
-    def visit_Expr(self, node):
+    def mutate_Expr(self, node):
         return ast.Pass()
  
        
@@ -231,32 +231,32 @@ class ConditionNegation(MutationOperator):
         node.test = not_node
         return node
     
-    def visit_While(self, node):
+    def mutate_While(self, node):
         return self.negate_test(node)
     
-    def visit_If(self, node):
+    def mutate_If(self, node):
         return self.negate_test(node)
 
     
 class SliceIndexReplace(MutationOperator):
     
-    def visit_Slice(self, node):
+    def mutate_Slice(self, node):
         node.lower, node.upper, node.step = node.upper, node.step, node.lower
         return node
 
     
 class MembershipTestReplacement(MutationOperator):
     
-    def visit_In(self, node):
+    def mutate_In(self, node):
         return ast.NotIn()
     
-    def visit_NotIn(self, node):
+    def mutate_NotIn(self, node):
         return ast.In()
 
     
 class ExceptionHandleDeletion(MutationOperator):
     
-    def visit_ExceptHandler(self, node):
+    def mutate_ExceptHandler(self, node):
         return None
 
 
@@ -266,10 +266,10 @@ class ZeroIterationLoop(MutationOperator):
         node.body = [ast.Break()]
         return node
     
-    def visit_For(self, node):
+    def mutate_For(self, node):
         return self.zero_iteration(node)
     
-    def visit_While(self, node):
+    def mutate_While(self, node):
         return self.zero_iteration(node)
 
 
@@ -279,16 +279,16 @@ class OneIterationLoop(MutationOperator):
         node.body.append(ast.Break())
         return node
     
-    def visit_For(self, node):
+    def mutate_For(self, node):
         return self.one_iteration(node)
     
-    def visit_While(self, node):
+    def mutate_While(self, node):
         return self.one_iteration(node)
     
     
 class ReverseIterationLoop(MutationOperator):
     
-    def visit_For(self, node):
+    def mutate_For(self, node):
         old_iter = node.iter
         node.iter = ast.Call(func=ast.Name(id=reversed.__name__, ctx=ast.Load()),
                              args=[old_iter], keywords=[], starargs=None, kwargs=None)
@@ -297,7 +297,7 @@ class ReverseIterationLoop(MutationOperator):
 
 class SelfWordDeletion(MutationOperator):
     
-    def visit_Attribute(self, node):
+    def mutate_Attribute(self, node):
         try:
             if node.value.id == 'self':
                 return ast.Name(id=node.attr, ctx=ast.Load())
