890c08ace0de6d66c1afc05f75e1af086a52fe43
khalas <k.halas@sunscrapers.com>
2011-11-24 23:17:14 +0100

    Moved sys.path.extend from bin/mut.py to mutpy/controller.py.

--- bin/mut.py
+++ bin/mut.py
@@ -1,9 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import sys
-sys.path.extend(['', '../'])
-
 from mutpy import commandline
 
 if __name__ == '__main__':
 	commandline.main(sys.argv)
+
