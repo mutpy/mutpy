70c712b1528c015a7ddebf7a3e8584af13960192
khalas <halas.konrad@gmail.com>
2013-09-25 00:00:23 +0200

    Fixed indention.

--- setup.py
+++ setup.py
@@ -7,20 +7,23 @@ if sys.hexversion < 0x3020000:
     print('MutPy requires Python 3.2 or newer!')
     sys.exit()
 
-setup(name='MutPy',
-      version='0.3.2',
-      description='Mutation testing tool for Python 3.x source code.',
-      author='Konrad Hałas',
-      author_email='halas.konrad@gmail.com',
-      url='https://bitbucket.org/khalas/mutpy',
-      download_url='https://bitbucket.org/khalas/mutpy',
-      packages=['mutpy'],
-      scripts=['bin/mut.py'],
-      install_requires=['PyYAML>=3.1'],
-      test_suite='mutpy.test',
-      classifiers=['Programming Language :: Python :: 3.2',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'Topic :: Software Development :: Testing',
-                   'License :: OSI Approved :: Apache Software License'])
-
+setup(
+    name='MutPy',
+    version='0.3.2',
+    description='Mutation testing tool for Python 3.x source code.',
+    author='Konrad Hałas',
+    author_email='halas.konrad@gmail.com',
+    url='https://bitbucket.org/khalas/mutpy',
+    download_url='https://bitbucket.org/khalas/mutpy',
+    packages=['mutpy'],
+    scripts=['bin/mut.py'],
+    install_requires=['PyYAML>=3.1'],
+    test_suite='mutpy.test',
+    classifiers=[
+        'Programming Language :: Python :: 3.2',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
