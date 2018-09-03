5638211315eed030058c098fb2ad19663be8747b
Philipp Hossner <philipp.hosner@posteo.de>
2018-09-03 12:58:08 +0200

    Add mutpy.operators to packages in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='halas.konrad@gmail.com',
     url='https://github.com/mutpy/mutpy',
     download_url='https://github.com/mutpy/mutpy',
-    packages=['mutpy'],
+    packages=['mutpy', 'mutpy.operators'],
     scripts=['bin/mut.py'],
     install_requires=requirements,
     test_suite='mutpy.test',
